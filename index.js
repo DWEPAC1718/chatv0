8594bdfb0e42a58e33734291520cc900d1556147
pedromoreira-estg <pmoreira.estg@gmail.com>
2018-01-11 19:35:47 +0000

    socketio

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 
+
+// usar um servidor http
+var http = require('http');
+var socketio = require("socket.io");
+
+
+
 var app = express();
 
 // path usado para obter o caminho absoluto de 'content' 
 // onde __dirname representa o caminho corrente
 // porque os caminhos são sempre relativos ao caminho
 // onde se inicia o processo do node 
+
+var server = http.createServer(app);
+var io = socketio.listen(server);
 
 var path = require('path');
 app.use(express.static(path.resolve(__dirname, 'content')));
@@ -23,5 +33,4 @@ app.get("/ola", function (req,res) {
 	res.send("<h1>Olá Domingos Escuteiro Gomes</h1>")
 });
 
-
-app.listen(3000);
+server.listen(3000);
